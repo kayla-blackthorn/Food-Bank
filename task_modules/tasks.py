eedd71e82e117647496eb09188302ac139b698f8
kayla-blackthorn <106624557+kayla-blackthorn@users.noreply.github.com>
2024-01-10 12:54:04 -0500

    test

--- task_modules/tasks.py
+++ task_modules/tasks.py
@@ -2,22 +2,22 @@ from cumulusci.tasks.command import Command
 
 class PostToSlackCommand(Command):
     task_options = {
-        "version_id": {
-            "description": "The version id to post to Slack",
+        "test_variable": {
+            "description": "The version number to post to Slack",
             "required": True,
         },
-        "version_number": {
+        "test_variable2": {
             "description": "The version number to post to Slack",
             "required": True,
         },
     }
 
     def _run_task(self):
-        message = f'Successfully Created Kayla Test Package Version: {self.options["version_number"]}\n'
-        message += 'Install URL: https://login.salesforce.com/packaging/installPackage.apexp?p0={}'.format(self.options['version_id'])
+        message = f'Successfully Created Kayla Test Package Version: {self.options["test_variable"]}\n'
+        message += 'Install URL: https://login.salesforce.com/packaging/installPackage.apexp?p0={}'.format(self.options['test_variable2'])
         self.options["command"] = (
             'curl -X POST -H "Content-Type: application/json" '
             '-d \'{{"text":"{}"}}\' '
-            'https://hooks.slack.com/services/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'.format(message)
+            'https://hooks.slack.com/services/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'.format(message)
         )
         super()._run_task()
